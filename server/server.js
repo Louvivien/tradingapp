ee4ce6cb0e630314021af2fa85c53bbfafb4c9a4
Oktarian <oktotb@gmail.com>
2020-09-19 22:38:34 +0200

    Heroku fix

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "../client/build/index.html"));
+    res.sendFile(path.join(__dirname + "/../client/build/index.html"));
   });
 }
 
