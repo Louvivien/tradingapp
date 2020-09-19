fd777ab3aa6745b13898523177a0b92d4204ff8f
Oktarian <oktotb@gmail.com>
2020-09-19 13:54:22 +0200

    UI change

--- client/src/components/Template/PageTemplate.jsx
+++ client/src/components/Template/PageTemplate.jsx
@@ -3,7 +3,6 @@ import { useHistory } from "react-router-dom";
 import UserContext from "../../context/UserContext";
 import styles from "./PageTemplate.module.css";
 import clsx from "clsx";
-import useStyles from "./styles";
 import {
   Drawer,
   CssBaseline,
@@ -23,6 +22,48 @@ import News from "../News/News";
 import Search from "../Search/Search";
 import SettingsModal from "./SettingsModal";
 import Axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  appBarSpacer: theme.mixins.toolbar,
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9),
+    },
+  },
+}));
 
 const PageTemplate = () => {
   const history = useHistory();
@@ -80,65 +121,68 @@ const PageTemplate = () => {
   return (
     <div className={styles.root}>
       <CssBaseline />
-      <AppBar
-        position="absolute"
-        className={clsx(
-          styles.appBarBackground,
-          classes.appBar,
-          open && classes.appBarShift
-        )}
-      >
-        <Toolbar className={styles.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(styles.menuButton, open && styles.menuButtonHidden)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
-            noWrap
-            className={styles.title}
-          >
-            {currentPage === "dashboard" && "Dashboard"}
-            {currentPage === "news" && "Market News"}
-            {currentPage === "search" && "Search"}
-          </Typography>
-          <Typography color="inherit">
-            Hello,{" "}
-            {userData.user.username
-              ? userData.user.username.charAt(0).toUpperCase() +
-                userData.user.username.slice(1)
-              : ""}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-        }}
-        open={open}
-      >
-        <div className={styles.toolbarIcon}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
-        </List>
-        <Divider />
-        <List>
-          <SecondNavbar logout={logout} openSettings={openSettings} />
-        </List>
-      </Drawer>
+        <AppBar
+          position="absolute"
+          className={clsx(
+            styles.appBarBackground,
+            classes.appBar,
+            open && classes.appBarShift
+          )}
+        >
+          <Toolbar className={styles.toolbar}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              className={clsx(
+                styles.menuButton,
+                open && styles.menuButtonHidden
+              )}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              className={styles.title}
+            >
+              {currentPage === "dashboard" && "Dashboard"}
+              {currentPage === "news" && "Market News"}
+              {currentPage === "search" && "Search"}
+            </Typography>
+            <Typography color="inherit">
+              Hello,{" "}
+              {userData.user.username
+                ? userData.user.username.charAt(0).toUpperCase() +
+                  userData.user.username.slice(1)
+                : ""}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+          }}
+          open={open}
+        >
+          <div className={styles.toolbarIcon}>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+          </List>
+          <Divider />
+          <List>
+            <SecondNavbar logout={logout} openSettings={openSettings} />
+          </List>
+        </Drawer>
       <main className={styles.content}>
         <div className={classes.appBarSpacer} />
         {currentPage === "dashboard" && (
