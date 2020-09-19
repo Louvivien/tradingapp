f54d0239a8a68191aade87de3d186fa24f47e45e
Oktarian <oktotb@gmail.com>
2020-09-19 22:19:29 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -46,10 +46,10 @@ app.use("/api/news", newsRouter);
 app.use("/api/stock", stockRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    res.sendFile("../client/build/index.html");
   });
 }
 
