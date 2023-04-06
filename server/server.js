7fa21be6826e3f2e382743cbbc71f492ccade93b
louvivien <vivien.richaud@gmail.com>
2023-04-06 14:08:46 +0200

    cors fix

--- server/server.js
+++ server/server.js
@@ -15,6 +15,11 @@ dotenv.config({ path: "./server/config/.env" });
 
 // Middleware
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://tradingapp-rust.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser("secretcode"));
