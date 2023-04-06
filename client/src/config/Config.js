4d7f68414e142dbbe5e1d3db6ba6ecfc84423b84
louvivien <vivien.richaud@gmail.com>
2023-04-06 19:39:20 +0200

    render test

--- client/src/config/Config.js
+++ client/src/config/Config.js
@@ -1,5 +1,8 @@
 const config = {};
 
+console.log("NODE_ENV:", process.env.NODE_ENV);
+
+
 if (process.env.NODE_ENV === 'production') {
   config.base_url = process.env.REACT_APP_BASE_URL_PROD;
 } else {
