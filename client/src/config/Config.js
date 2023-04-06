ff560c004602a9f5852a96ab20a478f7aba10510
louvivien <vivien.richaud@gmail.com>
2023-04-06 19:43:14 +0200

    render test

--- client/src/config/Config.js
+++ client/src/config/Config.js
@@ -4,9 +4,9 @@ console.log("NODE_ENV:", process.env.NODE_ENV);
 
 
 if (process.env.NODE_ENV === 'production') {
-  config.base_url = process.env.REACT_APP_BASE_URL_PROD;
+  config.base_url = 'https://tradingapp-divp.onrender.com';
 } else {
-  config.base_url = process.env.REACT_APP_BASE_URL_DEV;
+  config.base_url = 'http://localhost:5000';
 }
 
 export default config;
