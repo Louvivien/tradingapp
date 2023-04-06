ab64fb701658b3569aaaa1ee98e1ef31628c618a
louvivien <vivien.richaud@gmail.com>
2023-04-06 14:29:16 +0200

    cors fix

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const port = process.env.PORT || 5000;
 dotenv.config({ path: "./server/config/.env" });
 
 // Middleware
+app.use(cors());
 app.options("*", cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
