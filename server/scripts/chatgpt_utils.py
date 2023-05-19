d2b78564e377d4811eb83d5b11bf5d23063e67e4
louvivien <vivien.richaud@gmail.com>
2023-05-19 13:52:58 +0200

    fix strategies render

--- server/scripts/chatgpt_utils.py
+++ server/scripts/chatgpt_utils.py
@@ -10,6 +10,8 @@ from selenium.webdriver.common.keys import Keys
 import selenium.common.exceptions as Exceptions
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+
 
 
 
@@ -20,7 +22,8 @@ logging.basicConfig(
     level=logging.INFO  
 )
 
-
+options = webdriver.ChromeOptions()
+options.binary_location = '/opt/render/project/.render/chrome/opt/google/chrome/google-chrome'
 
 
 class ChatGPT_Client:
