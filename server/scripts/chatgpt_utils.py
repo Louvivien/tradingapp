590fcba36e73510fcec2771edcb0da5f6396ea35
louvivien <vivien.richaud@gmail.com>
2023-05-19 14:33:51 +0200

    fix strategies render

--- server/scripts/chatgpt_utils.py
+++ server/scripts/chatgpt_utils.py
@@ -21,7 +21,7 @@ options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox') 
 options.add_argument('start-maximized')
 options.add_argument('disable-infobars')
-options.add_argument('--headless=new')
+# options.add_argument('--headless=new')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--remote-debugging-port=9222')
 options.add_argument('--disable-browser-side-navigation')
@@ -71,13 +71,14 @@ class ChatGPT_Client:
             logging.info('Verbose mode active')
         options = uc.ChromeOptions()
         options.add_argument('--incognito')
-        if headless:
-            options.add_argument('--headless')
+        # if headless:
+        #     options.add_argument('--headless')
 
         logging.info('Loading undetected Chrome')
         self.browser = uc.Chrome(
+            use_subprocess=True,
             options=options,  
-            headless=headless,
+            # headless=headless,
             version_main=112
         )
         self.browser.set_page_load_timeout(30)
