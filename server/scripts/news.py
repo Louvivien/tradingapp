2b85efa55e9147e160d987032013676cdb45e9fb
louvivien <vivien.richaud@gmail.com>
2023-07-08 00:45:59 +0200

    remove duplicate and format json

--- server/scripts/news.py
+++ server/scripts/news.py
@@ -11,6 +11,8 @@ import hashlib
 import sys
 import logging
 import argparse
+from Levenshtein import distance as levenshtein_distance
+
 
 
 
@@ -144,11 +146,56 @@ def fetch_google_news(ticker='AAPL', period=1):
         return []
 
 
-def remove_duplicates(news_list):
-    """Remove duplicates from the list of news articles."""
-    news_dict = {generate_id(news['title'], news['date']): news for news in news_list}
-    return list(news_dict.values())
 
+# # Process the data
+
+# def remove_duplicates(news_list):
+#     """Remove duplicates from the list of news articles."""
+#     news_dict = {generate_id(news['title'], news['date']): news for news in news_list}
+#     return list(news_dict.values())
+
+
+def calculate_similarity(string1, string2):
+    """Calculate the similarity between two strings using the OSA distance."""
+    # Calculate the Levenshtein distance
+    distance = levenshtein_distance(string1, string2)
+    # Calculate the maximum possible distance
+    max_distance = max(len(string1), len(string2))
+    # Calculate the similarity
+    similarity = 1 - distance / max_distance
+    return similarity
+
+def remove_similar_headlines(news_list, similarity_threshold=0.6):
+    logging.info("Removing similar headlines...")
+    """Remove headlines with a similarity greater than the specified threshold."""
+    # Initialize an empty list to store the unique news
+    unique_news = []
+    # Initialize a counter for the number of removed headlines
+    removed_headlines = 0
+    # Loop over the news in the list
+    for news in news_list:
+        # Assume that the news is unique until proven otherwise
+        is_unique = True
+        # Loop over the unique news
+        for unique in unique_news:
+            # If the news is for the same company on the same day
+            if news['ticker'] == unique['ticker'] and news['date'].date() == unique['date'].date():
+                # Calculate the similarity between the headlines
+                similarity = calculate_similarity(news['title'], unique['title'])
+                # If the similarity is greater than the threshold
+                if similarity > similarity_threshold:
+                    # The news is not unique
+                    is_unique = False
+                    # logging.info(f"Removing duplicate headline: {news['title']}")
+                    # logging.info(f"Similarity with headline: {unique['title']}")
+                    removed_headlines += 1
+                    break
+        # If the news is unique
+        if is_unique:
+            # Add it to the list of unique news
+            unique_news.append(news)
+    logging.info(f"Total similar headlines removed: {removed_headlines}")
+    return unique_news
 
 
 def print_news_headlines(tickertick_news, google_news):
@@ -182,11 +229,11 @@ def main(ticker='AAPL', period=1):
     # Combine the news from both sources into a single list
     news_data = tickertick_news + google_news
 
-    # Combine the news from both sources into a single list
-    news_data = tickertick_news + google_news
+    # Remove similar headlines
+    news_data = remove_similar_headlines(news_data)
     
     try:
-        json_output = json.dumps(news_data, cls=DateTimeEncoder, ensure_ascii=False)
+        json_output = json.dumps(news_data, cls=DateTimeEncoder, ensure_ascii=False, indent=4)
         
         # Save the JSON output to the data folder
         file_path = os.path.join('..', 'data', 'newsData.json')
